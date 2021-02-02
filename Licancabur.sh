6d02bf0292bdb8b9edc7eab129b41caa5b2a6ac1
qvshuo <qvshuo@foxmail.com>
2021-02-02 23:18:36 +0800

    cover

--- Licancabur.sh
+++ Licancabur.sh
@@ -4,14 +4,14 @@ time=$(date +%H)
 dir="/home/huizhi/Pictures/backgrounds/Licancabur"
 
 if [ ${time} -ge 0 ] && [ ${time} -lt 6 ]; then
-	hsetroot -fill ${dir}/2.jpg &
+	hsetroot -cover ${dir}/2.jpg &
 fi
 
 if [ ${time} -ge 6 ] && [ ${time} -lt 18 ]; then
-	hsetroot -fill "${dir}"/1.jpg &
+	hsetroot -cover "${dir}"/1.jpg &
 fi
 
 if [ ${time} -ge 18 ] && [ ${time} -lt 24 ]; then
-	hsetroot -fill "${dir}"/2.jpg &
+	hsetroot -cover "${dir}"/2.jpg &
 fi
 
--- Your_Name.sh
+++ Your_Name.sh
@@ -4,37 +4,37 @@ time=$(date +%H)
 dir="/home/huizhi/Pictures/backgrounds/Your_Name"
 
 if [ ${time} -ge 0 ] && [ ${time} -lt 5 ]; then
-	hsetroot -fill "${dir}"/1.jpg &
+	hsetroot -cover "${dir}"/1.jpg &
 fi
 
 if [ ${time} -ge 5 ] && [ ${time} -lt 6 ]; then
-	hsetroot -fill ${dir}/2.jpg &
+	hsetroot -cover ${dir}/2.jpg &
 fi
 
 if [ ${time} -ge 6 ] && [ ${time} -lt 7 ]; then
-	hsetroot -fill ${dir}/3.jpg &
+	hsetroot -cover ${dir}/3.jpg &
 fi
 
 if [ ${time} -ge 7 ] && [ ${time} -lt 8 ]; then
-	hsetroot -fill ${dir}/4.jpg &
+	hsetroot -cover ${dir}/4.jpg &
 fi
 
 if [ ${time} -ge 8 ] && [ ${time} -lt 16 ]; then
-	hsetroot -fill ${dir}/5.jpg &
+	hsetroot -cover ${dir}/5.jpg &
 fi
 
 if [ ${time} -ge 16 ] && [ ${time} -lt 17 ]; then
-	hsetroot -fill ${dir}/4.jpg &
+	hsetroot -cover ${dir}/4.jpg &
 fi
 
 if [ ${time} -ge 17 ] && [ ${time} -lt 18 ]; then
-	hsetroot -fill ${dir}/3.jpg &
+	hsetroot -cover ${dir}/3.jpg &
 fi
 
 if [ ${time} -ge 18 ] && [ ${time} -lt 19 ]; then
-	hsetroot -fill ${dir}/2.jpg &
+	hsetroot -cover ${dir}/2.jpg &
 fi
 
 if [ ${time} -ge 19 ] && [ ${time} -lt 24 ]; then
-	hsetroot -fill ${dir}/1.jpg &
+	hsetroot -cover ${dir}/1.jpg &
 fi
